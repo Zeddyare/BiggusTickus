960622290e23c674b492cf937c5e40106daadd71
Zeddyare <w0433704@nscc.ca>
2025-09-19 17:55:48 -0300

    Updated Program Code

---
+++ Models/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace BiggusTickus.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Venue { get; set; } = string.Empty;
+        public List<Ticket>? Tickets { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
